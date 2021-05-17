e56b3e0945bee4340ce065685c6061c91ea64df7
Frederik <freumert@gmail.com>
2021-05-17 18:14:50 +0200

    test

--- app/src/main/java/com/example/exam_project/Adapters/CravingAdapter.java
+++ app/src/main/java/com/example/exam_project/Adapters/CravingAdapter.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CravingAdapter extends RecyclerView.Adapter<CravingAdapter.ViewHolder> {
-//sssss
+//sssssss
     private List<Craving> cravingList = new ArrayList<>();
     final private OnListItemClickListener mOnListItemClickListener;
     public CravingAdapter(OnListItemClickListener listener){
