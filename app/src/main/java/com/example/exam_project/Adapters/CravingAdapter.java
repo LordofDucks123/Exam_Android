028e0d4294aed4bfcfed2d858d9d64f4b3e4812c
LordofDucks123 <freumert@gmail.com>
2021-05-18 13:32:03 +0200

    Removed comment

--- app/src/main/java/com/example/exam_project/Adapters/CravingAdapter.java
+++ app/src/main/java/com/example/exam_project/Adapters/CravingAdapter.java
@@ -19,7 +19,7 @@ public class CravingAdapter extends RecyclerView.Adapter<CravingAdapter.ViewHold
     public CravingAdapter(OnListItemClickListener listener){
         mOnListItemClickListener = listener;
     }
-//Okays
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
